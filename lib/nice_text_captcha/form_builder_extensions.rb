836ca8e3c2860032b3f1ed93c59dab747ca4fdc1
Neil Smith <neil.jx.smith@gmail.com>
2011-09-20 10:18:48 +0100

    Allow :exclude_label argument to allow view helper to generate questions not wrapped in <label> tags

--- lib/nice_text_captcha/form_builder_extensions.rb
+++ lib/nice_text_captcha/form_builder_extensions.rb
@@ -2,10 +2,15 @@ module NiceTextCaptcha
   
   module FormBuilderExtensions
     
-    def nice_text_captcha
+    def nice_text_captcha(options = {})
       captcha = NiceTextCaptcha::Captcha.new
       @template.flash[:nice_text_captcha_responses] = captcha.encrypted_answers_for(@object_name)
-      label :nice_text_captcha, captcha.question
+      
+      if options[:exclude_label] and options[:exclude_label] == true
+        captcha.question
+      else
+        label :nice_text_captcha, captcha.question
+      end
     end
     
   end
